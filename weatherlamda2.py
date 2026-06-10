99a04ce37a738d618d0697f7cceb9c10b3de3270
Fathimatech <fathimapsfathima550@gmail.com>
2026-06-10 12:28:27 +0530

    Created Lambda function to transfer DynamoDB stream data to S3

---
+++ weatherlamda2.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+import uuid
+
+s3 = boto3.client('s3')
+
+BUCKET_NAME = 'weather-data-bt'
+
+def lambda_handler(event, context):
+
+    for record in event['Records']:
+        data = record['dynamodb']['NewImage']
+
+        file_name = f"{uuid.uuid4()}.json"
+
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=file_name,
+            Body=json.dumps(data)
+        )
+
+    return {
+        'statusCode': 200
+    }
